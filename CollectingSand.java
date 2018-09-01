455c7940778d4762c27935210178a61ef0c5ca75
DarylFletcher00 <39103514+DarylFletcher00@users.noreply.github.com>
2018-08-31 23:19:51 -0700

    Add files via upload

---
+++ CollectingSand.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+/* 1. Prompt user for amount of sand
+ * 	-if over 6000 make 220 loops, if less do num/26
+ * 2. Message Box telling user that recording will start on the next click
+ * 3. Record each click/right click and store in an array
+ * 	-take xPos, yPos, and time since last click
+ * 4. Information in array is used in a double for loop
+ * 	-First loop keeps track of how many total loops
+ * 	-Second loop goes through array and does the process of clicking and shit
+ * 5. Maybe make a private boolean variable to stop loop
+ * 
+ * Functions needed:
+ * 	-getX
+ * 	-getY
+ * 	-getTime
+ * 	-resetTimer
+ * 	-actionListener
+ * 		-mouse
+ * 		-keys
+ * 	-stopLoop
+ * 	-resetClicks
+ */
+
+// try {
+            // // These coordinates are screen coordinates
+            // int xCoord = 500;
+            // int yCoord = 500;
+
+            // // Move the cursor
+            // Robot robot = new Robot();
+            // robot.mouseMove(xCoord, yCoord);
+        // } catch (AWTException e) {
+        // }
+
+public class CollectingSand {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
